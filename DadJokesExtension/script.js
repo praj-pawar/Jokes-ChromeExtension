4baf7827e9e77e6099cc0875965369eb52d8e10d
Prajwal Pawar <prajpawar23@gmail.com>
2023-01-28 23:23:29 +0530

    Add files via upload

---
+++ DadJokesExtension/script.js
@@ -0,0 +1,8 @@
+fetch("https://icanhazdadjoke.com/slack")
+    .then(data => data.json())
+    .then(jokedata =>
+        {
+            const joketext=jokedata.attachments[0].text;
+            const jokeElement=document.getElementById("jokeElement");
+            jokeElement.innerHTML=joketext;
+        })
